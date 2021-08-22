74f05cb5ef3ada11bad3b89fc085fba9cf3a38a3
NieGuanglin <15139165731@163.com>
2021-08-22 18:26:58 +0800

    dev: bi_tree

---
+++ tree/bi_tree/bi_tree.go
@@ -0,0 +1,20 @@
+package bi_tree
+
+type biTree struct {
+	data interface{}
+	lChild *biTree
+	rChild *biTree
+}
+
+func (bi *biTree) PreOrderTraverse() {
+
+}
+
+func (bi *biTree) InOrderTraverse() {
+
+}
+
+func (bi *biTree) PostOrderTraverse() {
+
+}
+
