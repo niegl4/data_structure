e4460bb8cf7a3de25d9733d28d9431d7c7eeb441
NieGuanglin <15139165731@163.com>
2021-10-27 15:02:25 +0800

    dev: quick search

--- sort/05_quick/quick_test.go
+++ sort/05_quick/quick_test.go
@@ -38,3 +38,25 @@ func TestQuickSort2(t *testing.T) {
 	t.Log(res)
 }
 
+func TestQuickSearch(t *testing.T) {
+	//第1大为：12
+	//第2大为：5
+	//第3大为：4
+	//第4大为：3
+	//第5大为：2
+	resMap := map[int]int {
+		1: 12,
+		2: 5,
+		3: 4,
+		4: 3,
+		5: 2,
+	}
+	for i := 1; i <= 5; i++ {
+		testArr := []int{4, 2, 5, 12, 3}
+		ele := QuickSearch(testArr, i)
+		if ele != resMap[i] {
+			t.Fatal("search failed")
+		}
+		t.Log(ele)
+	}
+}
