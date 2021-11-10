504f6904ade85fea42b0042cdb2031189a507e3a
NieGuanglin <15139165731@163.com>
2021-11-10 18:04:03 +0800

    modify: heap sort

--- sort/06_heap/heap.go
+++ sort/06_heap/heap.go
@@ -1,6 +1,6 @@
 package _6_heap
 
-//sort by ascend, a index begin from 1, has n elements
+//做升序排序，建大顶堆。从最后一个有子节点的节点开始，自上向下堆化。这里从1号索引开始存数据，n表示一共几个数据。
 func HeapSort(a []int, n int) []int {
 	buildHeap(a, n)
 
@@ -13,27 +13,25 @@ func HeapSort(a []int, n int) []int {
 	return a
 }
 
-//build a heap
+//从最后一个有子节点的节点开始，自上向下堆化。
 func buildHeap(a []int, n int) {
-
 	//heapify from the last parent node
 	for i := n / 2; i >= 1; i-- {
 		heapifyUpToDown(a, i, n)
 	}
-
 }
 
 //heapify from up to down , node index = top
 func heapifyUpToDown(a []int, top int, count int) {
 
-	for i := top; i <= count/2; {
+	for i := top; i <= count/2; { //必须小于等于count/2，大于则节点没有子节点
 
 		maxIndex := i
-		if a[i] < a[i*2] {
+		if a[i] < a[i*2] { //与左子节点比较
 			maxIndex = i * 2
 		}
 
-		if i*2+1 <= count && a[maxIndex] < a[i*2+1] {
+		if i*2+1 <= count && a[maxIndex] < a[i*2+1] { //与右子节点比较
 			maxIndex = i*2 + 1
 		}
 
@@ -41,7 +39,7 @@ func heapifyUpToDown(a []int, top int, count int) {
 			break
 		}
 
-		swap(a, i, maxIndex)
+		a[i], a[maxIndex] = a[maxIndex], a[i]
 		i = maxIndex
 	}
 
