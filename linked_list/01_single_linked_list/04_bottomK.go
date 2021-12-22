c07b80105f9a098f1693529b8067e1a2735caec2
NieGuanglin <15139165731@163.com>
2021-12-22 21:47:22 +0800

    dev: delete bottom k

--- linked_list/01_single_linked_list/04_bottomK.go
+++ linked_list/01_single_linked_list/04_bottomK.go
@@ -1,7 +1,8 @@
 package _1_single_linked_list
 
 /*
-删除倒数第k个节点：
+二十二-1
+获取倒数第k个节点：
 
 先获取正数第N个节点，再快、慢指针一起移动。这种做法很好的平衡了N接近链表尾部/头部的情况。
  */
@@ -15,7 +16,7 @@ func getBottomK(head *ListNode, k int) *ListNode {
 		fast = fast.next
 	}
 	//检测上方循环退出的原因。满足下面条件，说明list节点数小于k。
-	if fast.next == nil && i < k-1 {
+	if fast.next == nil && i < k {
 		return nil
 	}
 
@@ -27,47 +28,51 @@ func getBottomK(head *ListNode, k int) *ListNode {
 	return slow
 }
 
-func delBottomK(head **ListNode, k int) {
+/*
+二十二-2
+删除倒数第k个节点：
+
+先获取正数第N个节点，再快、慢指针一起移动。这种做法很好的平衡了N接近链表尾部/头部的情况。
+*/
+//v1相当于"获取倒数第k个节点"+"O(1)时间删除节点"的组合。缺点是需要多次遍历，而且要实现两个子函数。
+func delBottomKV1(head **ListNode, k int) {
+	bottomKNode := getBottomK(*head, k)
+	if bottomKNode == nil {
+		return
+	}
+	_ = DelNode(head, bottomKNode)
+	return
+}
+
+//v2不用多次遍历链表
+func delBottomKV2(head **ListNode, k int) {
 	if head == nil || *head == nil || k == 0 {
 		return
 	}
 	fast := *head
-	for i := 1; i <= k && fast.next != nil; i++ {
+	i := 1
+	for ; i <= k-1 && fast.next != nil; i++ {
 		fast = fast.next
 	}
-	if fast == nil {
+	//检测上方循环退出的原因。满足下面条件，说明list节点数小于k。
+	if fast.next == nil && i < k {
 		return
 	}
 
+	if fast.next == nil {//等价于删除头节点。eg：三个节点，删除倒数第3个。
+		*head = (*head).next
+		return
+	}
+
+	pre := *head
 	slow := *head
 	for fast.next != nil {
 		fast = fast.next
+
+		pre = slow
 		slow = slow.next
 	}
-	slow.next = slow.next.next
+	pre.next = slow.next
 	return
 }
 
-func (l *LinkedList) DeleteBottomK(n int) {
-	if n <= 0 || l == nil || l.head == nil || l.head.next == nil {
-		return
-	}
-
-	//先获取正数第N个节点
-	fast := l.head //从head开始，那么结束的时候，fast刚好就是第n个节点
-	for i := 1; i <= n && fast != nil; i++ {
-		fast = fast.next
-	}
-	//上面循环结束的原因，如果是fast为nil，而不是计数到达，那么说明链表内没有倒数第N个节点
-	if fast == nil {
-		return
-	}
-
-
-	slow := l.head
-	for fast.next != nil {//fast为nil的时候，slow刚好是倒数第N+1个节点，方便删除。
-		slow = slow.next
-		fast = fast.next
-	}
-	slow.next = slow.next.next
-}
--- linked_list/01_single_linked_list/04_bottomK_test.go
+++ linked_list/01_single_linked_list/04_bottomK_test.go
@@ -1,15 +1,35 @@
 package _1_single_linked_list
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetBottomK(t *testing.T) {
 	head := genList([]int{1, 2, 3, 4})
 	for i := 0; i < 6; i++ {
-		bottomKNode := getBottomK(head, i+10)
+		bottomKNode := getBottomK(head, i)
 		if bottomKNode == nil {
 			t.Log("nil")
 			continue
 		}
 		t.Log(bottomKNode.value)
+	}
+}
+
+func TestDelBottomK(t *testing.T) {
+	arrSet := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	for idx, arr := range arrSet {
+		head := genList(arr)
+		//delBottomKV1(&head, idx)
+		delBottomKV2(&head, idx)
+		printList(head)
+		fmt.Println("-------")
 	}
 }
