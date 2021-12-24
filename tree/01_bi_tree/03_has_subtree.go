69d9a07ac577d71ec3410054337840cf67fc9d11
NieGuanglin <15139165731@163.com>
2021-12-24 15:54:22 +0800

    dev: tree1 has tree2

---
+++ tree/01_bi_tree/03_has_subtree.go
@@ -0,0 +1,35 @@
+package _1_bi_tree
+
+/*
+二十六
+判断树2是不是树1的子结构
+ */
+func tree1HasTree2(root1, root2 *BiTree) bool {
+	if root1 == nil || root2 == nil {
+		return false
+	}
+
+	result := false
+	if (root1.Data).(int) == (root2.Data).(int) {
+		result = tree1HasTree2Core(root1, root2)
+	}
+	if !result {
+		result = tree1HasTree2(root1.lChild, root2)
+	}
+	if !result {
+		result = tree1HasTree2(root1.rChild, root2)
+	}
+	return result
+}
+
+func tree1HasTree2Core(node1, node2 *BiTree) bool {
+	if node2 == nil {
+		return true
+	} else if node1 == nil {
+		return false
+	}
+	if node1.Data.(int) != node2.Data.(int) {
+		return false
+	}
+	return tree1HasTree2Core(node1.lChild, node2.lChild) && tree1HasTree2Core(node1.rChild, node2.rChild)
+}
---
+++ tree/01_bi_tree/03_has_subtree_test.go
@@ -0,0 +1,27 @@
+package _1_bi_tree
+
+import "testing"
+
+func TestTree1HasTree2(t *testing.T) {
+	t1Node1 := &BiTree{Data: 8}
+	t1Node2 := &BiTree{Data: 8}
+	t1Node3 := &BiTree{Data: 7}
+	t1Node4 := &BiTree{Data: 9}
+	t1Node5 := &BiTree{Data: 2}
+	t1Node6 := &BiTree{Data: 4}
+	t1Node7 := &BiTree{Data: 7}
+	t1Node1.lChild = t1Node2
+	t1Node1.rChild = t1Node3
+	t1Node2.lChild = t1Node4
+	t1Node2.rChild = t1Node5
+	t1Node5.lChild = t1Node6
+	t1Node5.rChild = t1Node7
+
+	t2Node1 := &BiTree{Data: 8}
+	t2Node2 := &BiTree{Data: 9}
+	t2Node3 := &BiTree{Data: 2}
+	t2Node1.lChild = t2Node2
+	t2Node1.rChild = t2Node3
+
+	t.Log(tree1HasTree2(t1Node1, t2Node1))
+}
