0b1c09298a94a8da222f529558616483f2c8f216
NieGuanglin <15139165731@163.com>
2021-12-25 20:38:42 +0800

    dev: clone complex list

---
+++ linked_list/01_single_linked_list/08_clone_complex_list.go
@@ -0,0 +1,69 @@
+package _1_single_linked_list
+
+/*
+三十五
+复制复杂链表。复杂链表中，除了next指向下一节点，还有sibling指针指向任意节点或指向nil。
+
+最容易想到的办法是，先复制主干，再逐个处理sibling指针（从头遍历查找），空间复杂度O(n^2)
+借助散列表，在O(n)的空间复杂度下，时间复杂度被优化为O(n)
+下面的方法，不使用散列表，时间复杂度为O(n)
+*/
+
+type ComplexList struct {
+	Val     interface{}
+	Next    *ComplexList
+	Sibling *ComplexList
+}
+
+//1.每个原链表的节点后面，都插入一个copy节点
+//2.copy节点的sibling，指向它前一个节点的sibling的next
+//3.把copy节点从原链表中断开
+func cloneComplexList(head *ComplexList) *ComplexList {
+	if head == nil {
+		return nil
+	}
+	cloneNodes(head)
+	connectSibling(head)
+	return reconnectNodes(head)
+}
+
+func cloneNodes(head *ComplexList) {
+	node := head
+	for node != nil {
+		next := node.Next
+		node.Next = &ComplexList{
+			Val:  node.Val,
+			Next: next,
+		}
+		node = next
+	}
+}
+
+func connectSibling(head *ComplexList) {
+	node := head
+	for node != nil {
+		node.Next.Sibling = node.Sibling
+		node = node.Next.Next
+	}
+}
+
+func reconnectNodes(head *ComplexList) *ComplexList {
+	cloneHead := head.Next
+	node := head
+	cloneNode := cloneHead
+	for node != nil {
+		next := cloneNode.Next
+		if next == nil {
+			node.Next = nil
+			cloneNode.Next = nil
+			break
+		}
+		cloneNext := next.Next
+		node.Next = next
+		cloneNode.Next = cloneNext
+
+		node = node.Next
+		cloneNode = cloneNode.Next
+	}
+	return cloneHead
+}
---
+++ linked_list/01_single_linked_list/08_clone_complex_list_test.go
@@ -0,0 +1,51 @@
+package _1_single_linked_list
+
+import "testing"
+
+func TestCloneComplexList(t *testing.T) {
+	head := &ComplexList{
+		Val: "A",
+		Next: &ComplexList{
+			Val: "B",
+			Next: &ComplexList{
+				Val: "C",
+				Next: &ComplexList{
+					Val: "D",
+					Next: &ComplexList{
+						Val:     "E",
+						Next:    nil,
+						Sibling: nil,
+					},
+					Sibling: nil,
+				},
+				Sibling: nil,
+			},
+			Sibling: nil,
+		},
+		Sibling: nil,
+	}
+	head.Sibling = head.Next.Next
+	head.Next.Sibling = head.Next.Next.Next.Next
+	head.Next.Next.Next.Sibling = head.Next
+
+	newHead := cloneComplexList(head)
+	node := newHead
+	tmp := make([]interface{}, 0)
+	for node != nil {
+		tmp = append(tmp, node.Val)
+		node = node.Next
+	}
+	t.Log(tmp)
+
+	node = newHead
+	tmp = tmp[0:0]
+	for node != nil {
+		if node.Sibling == nil {
+			tmp = append(tmp, "nil")
+		} else {
+			tmp = append(tmp, node.Sibling.Val)
+		}
+		node = node.Next
+	}
+	t.Log(tmp)
+}
