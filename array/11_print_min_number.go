89aa8c4292c8e38ed24214a029f292f64279664a
NieGuanglin <15139165731@163.com>
2021-12-26 23:47:53 +0800

    dev: min number of arr

---
+++ array/11_print_min_number.go
@@ -0,0 +1,31 @@
+package array
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+/*
+四十五
+输入一个正整数，把数组里所有数字拼接起来排成一个数，输出所有能拼接的数字中最小的那个。
+*/
+
+func printMinNumber(arr []int) string {
+	if len(arr) <= 0 {
+		return ""
+	}
+
+	var arrOfStr []string
+	for _, b := range arr {
+		str := fmt.Sprintf("%d", b)
+		arrOfStr = append(arrOfStr, str)
+	}
+	sort.Slice(arrOfStr, func(i, j int) bool {
+		str1 := arrOfStr[i] + arrOfStr[j]
+		str2 := arrOfStr[j] + arrOfStr[i]
+		return str1 < str2
+	})
+
+	return strings.Join(arrOfStr, "")
+}
---
+++ array/11_print_min_number_test.go
@@ -0,0 +1,7 @@
+package array
+
+import "testing"
+
+func TestPrintMinNumber(t *testing.T) {
+	t.Log(printMinNumber([]int{3, 32, 321}))
+}
