a89b3e75f775d5be99e4977778b606b364faab7c
NieGuanglin <15139165731@163.com>
2021-12-30 10:00:29 +0800

    dev: translation count

---
+++ number/05_translation_count.go
@@ -0,0 +1,44 @@
+package number
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/*
+四十六
+给定整数，计算翻译的方式有多少种。0翻译为a,25翻译为z。
+
+（先把整数转换成字符串）
+*/
+
+func translationCount(number int) int {
+	if number < 0 {
+		return 0
+	}
+	numberStr := fmt.Sprintf("%d", number)
+	length := len(numberStr)
+	numberSli := make([]int32, length)
+	count := make([]int, length)
+	for _, n := range numberStr {
+		a := n - '0'
+		numberSli = append(numberSli, a)
+	}
+
+	for i := length - 1; i >= 0; i-- {
+		if i == length-1 {
+			count[i] = 1
+		} else {
+			count[i] = count[i+1]
+		}
+
+		if i <= length-3 {
+			num, _ := strconv.Atoi(numberStr[i : i+2])
+			if num >= 0 && num <= 25 {
+				count[i] += count[i+2]
+			}
+		}
+	}
+
+	return count[0]
+}
---
+++ number/05_translation_count_test.go
@@ -0,0 +1,8 @@
+package number
+
+import "testing"
+
+func TestTranslationCount(t *testing.T) {
+	t.Log(translationCount(12345))
+	t.Log(translationCount(12258))
+}
