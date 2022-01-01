6cc6b1e6a855808cf3c9a0d115c0225322874863
NieGuanglin <15139165731@163.com>
2022-01-01 15:41:24 +0800

    dev: count in sort array

---
+++ search/02_count_in_sort_array.go
@@ -0,0 +1,70 @@
+package search
+
+import "errors"
+
+/*
+五十三-1
+统计一个数字在排序数组中出现的次数。
+*/
+func countInSortArray(arr []int, k int) int {
+	if len(arr) < 1 {
+		return 0
+	}
+	start := BSearchExtend1(arr, k)
+	if start == -1 {
+		return 0
+	}
+	end := BSearchExtend2(arr, k)
+	return end - start + 1
+}
+
+/*
+五十四-2
+n-1长度的排序数组，每个数字都是唯一的，取值范围是0~n-1。求唯一一个在取值范围但不在数组中的数。
+*/
+func missNumInSortArray(arr []int) int {
+	n := len(arr)
+	if n < 1 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := (high-low)>>2 + low
+		if arr[mid] == mid {
+			low = mid + 1
+		} else {
+			if mid == 0 || arr[mid-1] == mid-1 {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+/*
+五十五-3
+一个单调递增的数组里，每个元素都是整数且唯一。求任一数值等于下标的元素。
+*/
+func numberEqualIndex(arr []int) (int, error) {
+	n := len(arr)
+	if n < 1 {
+		return 0, errors.New("input array invalid")
+	}
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := (high-low)>>2 + low
+		if arr[mid] == mid {
+			return mid, nil
+		} else if arr[mid] < mid {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return 0, errors.New("input array invalid")
+}
---
+++ search/02_count_in_sort_array_test.go
@@ -0,0 +1,15 @@
+package search
+
+import "testing"
+
+func TestCountInSortArray(t *testing.T) {
+	t.Log(countInSortArray([]int{1, 2, 3, 3, 3, 3, 4, 5}, 3))
+}
+
+func TestMissNumInSortArray(t *testing.T) {
+	t.Log(missNumInSortArray([]int{0, 1, 3, 4}))
+}
+
+func TestNumberEqualIndex(t *testing.T) {
+	t.Log(numberEqualIndex([]int{-3, -1, 1, 3, 5}))
+}
