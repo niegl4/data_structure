aeb9e3545448856531845d7d46cd33bf40168fb5
NieGuanglin <15139165731@163.com>
2022-01-02 17:38:39 +0800

    dev: swap with bit operate

--- bit_calculate/02_add.go
+++ bit_calculate/02_add.go
@@ -1,7 +1,7 @@
 package bit_calculate
 
 /*
-六十五
+六十五-1
 不用+，-，*，/，做加法。
 */
 func add(num1, num2 int) int {
@@ -16,4 +16,22 @@ func add(num1, num2 int) int {
 		num2 = carry
 	}
 	return num1
+}
+
+/*
+六十五-2
+不使用新变量，交换两个变量的值。
+*/
+func swapV1(num1, num2 int) (int, int) {
+	num1 = num1 + num2
+	num2 = num1 - num2
+	num1 = num1 - num2
+	return num1, num2
+}
+
+func swapV2(num1, num2 int) (int, int) {
+	num1 = num1 ^ num2
+	num2 = num1 ^ num2
+	num1 = num1 ^ num2
+	return num1, num2
 }
--- bit_calculate/02_add_test.go
+++ bit_calculate/02_add_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestAdd(t *testing.T) {
 	t.Log(add(5, 17))
 }
+
+func TestSwap(t *testing.T) {
+	t.Log(swapV1(1, 2))
+	t.Log(swapV2(1, 2))
+}
