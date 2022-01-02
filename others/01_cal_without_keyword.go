a8111eae37c06972bd8e63bdef218fad21c8b488
NieGuanglin <15139165731@163.com>
2022-01-02 17:24:48 +0800

    dev: calculate without keyword

---
+++ others/01_cal_without_keyword.go
@@ -0,0 +1,19 @@
+package others
+
+/*
+六十四
+不用乘除法，不用语法关键字，计算：1+2+...+n
+*/
+type sumWithOutKeyword func(int) int
+
+func terminator(n int) int {
+	return 0
+}
+
+func sum(n int) int {
+	fMap := map[bool]sumWithOutKeyword{
+		false: sum,
+		true:  terminator,
+	}
+	return n + fMap[n == 0](n-1)
+}
---
+++ others/01_cal_without_keyword_test.go
@@ -0,0 +1,7 @@
+package others
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Log(sum(5))
+}
